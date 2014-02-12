dd4367197cbb9995ef54cfc18c150e3284f51361
ghstreddy <guduru.thirupathireddy@gmail.com>
2014-02-11 22:27:46 -0600

    Added counters to mapreduce job

--- src/main/java/org/hadoop/mapreduce/MaxTempMapReduceJob.java
+++ src/main/java/org/hadoop/mapreduce/MaxTempMapReduceJob.java
@@ -15,30 +15,29 @@ import org.apache.hadoop.mapred.JobConf;
 
 /**
  * @author hadoop
- *
  */
 public class MaxTempMapReduceJob {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		if(args.length != 2){
-			System.out.println("Usage : MaxTempMapReduceJob <input path> <output path>");
+		if (args.length != 2) {
+			System.out.println("Usage : hadoop jar <jar> org.hadoop.mapreduce.MaxTempMapReduceJob <input path> <output path>");
 		}
 		JobConf jobConf = new JobConf(MaxTempMapReduceJob.class);
 		jobConf.setJobName("Max Temparature Job");
-		
+
 		FileInputFormat.addInputPath(jobConf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(jobConf, new Path(args[1]));
-		
+
 		jobConf.setMapperClass(MaxTemparatureMapper.class);
 		jobConf.setReducerClass(MaxTemparatureReducer.class);
-		
+
 		jobConf.setOutputKeyClass(Text.class);
 		jobConf.setOutputValueClass(IntWritable.class);
-		
+
 		JobClient.runJob(jobConf);
 	}
 
--- src/main/java/org/hadoop/mapreduce/MaxTemparatureMapper.java
+++ src/main/java/org/hadoop/mapreduce/MaxTemparatureMapper.java
@@ -4,7 +4,6 @@
 package org.hadoop.mapreduce;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -16,34 +15,43 @@ import org.apache.hadoop.mapred.Reporter;
 
 /**
  * @author hadoop
- *
  */
-public class MaxTemparatureMapper implements Mapper<LongWritable, Text, Text, IntWritable> {
+public class MaxTemparatureMapper implements
+                Mapper<LongWritable, Text, Text, IntWritable> {
 
-	@Override
-	public void configure(JobConf arg0) {
-		// TODO Auto-generated method stub
-		
+	private enum VALUE {
+		VALUE;
 	}
 
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void configure(JobConf arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void map(LongWritable key, Text value,
-			OutputCollector<Text, IntWritable> output, Reporter reporter)
-			throws IOException {
+	                OutputCollector<Text, IntWritable> output, Reporter reporter)
+	                throws IOException {
 		String line = value.toString();
 		System.out.println(" LINE ==== " + line);
 		String year = line.substring(0, 4);
 		System.out.println("YEAR === " + year);
-		
+
 		int lastIndex = line.lastIndexOf(',');
 		int maxTemp = Integer.parseInt(line.substring(lastIndex + 1));
+		reporter.setStatus("Map Executed");
+		reporter.incrCounter(VALUE.VALUE, 1);
+		if (maxTemp == 0) {
+			reporter.incrCounter("TEMP", "ZERO", 1);
+		}
 		output.collect(new Text(year), new IntWritable(maxTemp));
 	}
-	
+
 }
